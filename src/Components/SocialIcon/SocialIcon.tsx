9f868c419078a7ad82f6ca14caaeefb7e14c9e0a
road2paradise <ken_nggg@outlook.com>
2021-07-30 10:47:48 +1200

    added storybook and created socialicon story

--- src/Components/SocialIcon/SocialIcon.tsx
+++ src/Components/SocialIcon/SocialIcon.tsx
@@ -3,11 +3,23 @@ import { IconButton } from '@material-ui/core';
 
 
 export interface SocialIconProps {
+    /**
+     * Name of the social icon
+     */
     name: string,
+    /**
+     * URL that the social icon links to
+     */
     url: string,
+    /**
+     * Imported svg file for the social icon to render its logo.
+     */
     logo: string,
 }
 
+/**
+ * Social Icon used to render social media icons 
+ */
 export const SocialIcon: React.FC<SocialIconProps> = ({ name, url, logo }) => {
     return (
         <IconButton href={url}>
